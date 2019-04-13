8ae1870d140ed8080fd2dd9258c6dc15e5da0dc8
cetinerhalil <hhalilcetiner@gmail.com>
2019-04-14 01:55:24 +0300

    Update kuveyt redirect uri

--- api/config/kuveyt.js
+++ api/config/kuveyt.js
@@ -3,5 +3,5 @@ module.exports =  {
     client_id: '803d05743892496eac875fa83d49026a',
     client_secret: 'oNeRvaW3SqSvWJUGCl0Bv5KftP8FTk6WnibUf0wQAw1L45JmDQUZxw==',
     token: 'Basic ODAzZDA1NzQzODkyNDk2ZWFjODc1ZmE4M2Q0OTAyNmE6b05lUnZhVzNTcVN2V0pVR0NsMEJ2NUtmdFA4RlRrNlduaWJVZjB3UUF3MUw0NUptRFFVWnh3PT0=',
-    redirect_uri: 'https://api-finch.herokuapp.com/auth/kuveyt',
+    redirect_uri: 'https://apifinch.herokuapp.com/auth/kuveyt',
 }
