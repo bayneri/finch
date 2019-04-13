c9b921bdbc7d4300a8c01eb97f79ada310d8dcb6
cetinerhalil <hhalilcetiner@gmail.com>
2019-04-13 20:09:14 +0300

    Create transactions logic

---
+++ api/models/transactions.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TransactionSchema = new Schema({
+    user: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    totalAmount: {
+        type: Number,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        required: true
+    },
+    items: {
+        type: [Object]
+    }
+});
+
+const Transactions = mongoose.model('Transaction', TransactionSchema);
+
+module.exports = {
+    create: transaction => Transactions.create(transaction),
+    findOne: transaction => Transactions.findOne(transaction),
+    find: transaction => Transactions.find(transaction),
+    findOneAndUpdate: (query, params) => Transactions.findOneAndUpdate(query, params),
+    Transactions,
+};
