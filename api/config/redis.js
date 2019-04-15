7cff231dc2f5b61d941459caaff29da08537b12a
cetinerhalil <hhalilcetiner@gmail.com>
2019-04-15 12:02:50 +0300

    remove sensitive data

--- api/config/redis.js
+++ api/config/redis.js
@@ -1,6 +1,6 @@
 module.exports =  {
-    host: 'redis-15138.c16.us-east-1-3.ec2.cloud.redislabs.com',
-    port: '15138',
-    db: 'finch',
-    password: 'aTg2dyCmmpQsuhIvKX6XKADnagkPPMT6'
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    db: process.env.REDIS_NAME,
+    password: process.env.REDIS_PASS
 }
