ec399a8d709aaa3106a8f8535a6cc817efe8eca6
cetinerhalil <hhalilcetiner@gmail.com>
2019-04-15 12:02:50 +0300

    remove sensitive data

--- api/config/kuveyt.js
+++ api/config/kuveyt.js
@@ -1,7 +1,7 @@
 module.exports =  {
-    host: 'https://idprep.kuveytturk.com.tr/api/connect/token',
-    client_id: '803d05743892496eac875fa83d49026a',
-    client_secret: 'oNeRvaW3SqSvWJUGCl0Bv5KftP8FTk6WnibUf0wQAw1L45JmDQUZxw==',
-    token: 'Basic ODAzZDA1NzQzODkyNDk2ZWFjODc1ZmE4M2Q0OTAyNmE6b05lUnZhVzNTcVN2V0pVR0NsMEJ2NUtmdFA4RlRrNlduaWJVZjB3UUF3MUw0NUptRFFVWnh3PT0=',
-    redirect_uri: 'https://apifinch.herokuapp.com/auth/kuveyt',
+    host: process.env.KUVEYT_HOST,
+    client_id: process.env.KUVEYT_ID,
+    client_secret: process.env.KUVEYT_SECRET,
+    token: process.env.KUVEYT_TOKEN,
+    redirect_uri: process.env.KUVEYT_REDIRECT,
 }
